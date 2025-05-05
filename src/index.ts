450df5cd3badad2e1cb1f5f0208aa2bfdb5f518d
Syed Arman <syedarman2003@gmail.com>
2025-05-04 22:29:21 -0400

    Use dynamic port for Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import build from './app'
 import { FastifyInstance } from 'fastify'
 
+const PORT = parseInt(process.env.PORT || '8080', 10)
+
 const server: FastifyInstance = build({
   logger: { level: 'error' },
 })
 
 server
-  .listen({ port: 8080, host: '0.0.0.0' })
+  .listen({ port: PORT, host: '0.0.0.0' })
   .then(address => console.log(`Server listening at ${address}`))
   .catch(err => {
     console.error(err)
